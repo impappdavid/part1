afcb7c8029fbcab71f7bd5dc52740df18d24ee18
Papp Dávid <impappdavid@gmail.com>
2026-01-06 12:12:49 +0100

    Making the 1.9 part

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -4,7 +4,21 @@ const Header = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const Statistics = ({ good, neutral, bad, total }) => <div><p>good: {good}</p><p> neutral: {neutral}</p><p> bad: {bad}</p><p> all: {total}</p><p> average: {(good + (-bad)) / total}</p> <p> positive: {good / total}%</p></div>
+const Statistics = ({ good, neutral, bad, total }) => {
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+      <p>good: {good}</p>
+      <p> neutral: {neutral}</p>
+      <p> bad: {bad}</p>
+      <p> all: {total}</p>
+      <p> average: {(good + (-bad)) / total}</p>
+      <p> positive: {good / total}%</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
